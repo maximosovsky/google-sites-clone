f2aaa289a094da4ad1e255fa21b607dd8ae3e600
Maxim Osovsky <Maxim.Osovsky@gmail.com>
2026-03-09 15:42:16 +0300

    fix: use master branch for workflow dispatch

--- site/api/clone.js
+++ site/api/clone.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    ref: 'main',
+                    ref: 'master',
                     inputs: {
                         url,
                         email,
